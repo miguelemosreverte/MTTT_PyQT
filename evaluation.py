79e7a223a153a1adc7cbba9f8b40156aacc47aaf
miguelemosreverte <miguellemos95@gmail.com>
2017-05-19 10:49:33 -0300

    Added back the useful heuristic that caches, quite precisely, the results for each test and reference combination, even taking into account the modification date of the used files.

--- evaluation.py
+++ evaluation.py
@@ -23,6 +23,24 @@
 ##############################################################################
 import subprocess
 import os
+import platform
+
+def creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
 
 def filterTER (lines):
     result = ''
@@ -75,7 +93,7 @@ def evaluate(checkbox_indexes, hypothesis, reference):
     BLEU_cached_results = ""
     for checkbox in checkbox_indexes:
         if checkbox:
-            key = checkbox_indexes_constants[checkbox_index]
+            key = (test,creation_date(test),reference,creation_date(reference), checkbox_indexes_constants[checkbox_index])
             if key in cached_results: return_results += cached_results[key]
             else:
 
